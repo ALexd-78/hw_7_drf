1c16ccf39d95a6f5573d1e1c7f755c1d6892f639
ALexd-78 <alexd-78@yandex.ru>
2023-08-15 01:46:22 +0300

    modify access rights for owner and staff

--- training/views.py
+++ training/views.py
@@ -3,35 +3,47 @@ from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
 
 from training.models import Course, Lesson, Payments
+from training.permissions import IsOwnerOrStaff, IsOwner
 from training.serializers import CourseSerializer, LessonSerializer, PaymentsSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    permission_classes = [IsOwnerOrStaff]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsOwner]
 
+    def perform_create(self, serializer):
+        # переопределение метода создания объекта класса только для авторизованного пользователя
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner]
 
 
 class PaymentListAPIView(generics.ListAPIView):
--- users/models.py
+++ users/models.py
@@ -6,13 +6,18 @@ NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
-
+    name = models.CharField(max_length=200, verbose_name='фамилия имя отчество', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     city = models.CharField(max_length=150, verbose_name='город', **NULLABLE)
 
-    token = models.CharField(max_length=200, verbose_name='токен', **NULLABLE)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='время создания токена', **NULLABLE)
-
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ('email',)
